b5688e6e830af6e04ccd5210447f8b8dbc4815e3
AlexKarashi <60771777+AlexKarashi@users.noreply.github.com>
2022-11-29 10:13:09 -0600

    Add files via upload
    Correcion de errores, creacion de la pagina 6 , implementacion del primer ejercicio y aagregado de links a los botones

---
+++ apps/tarea6.js
@@ -0,0 +1,25 @@
+// se comenta con las diagonales
+
+function despedir(){
+    alert("Adios desde un JS");
+}
+
+function moral(){
+    alert("realmente no hay nada aqui, pa que clickeas")
+}
+
+console.log("aqui hace acto de presencia la consola");
+
+console.log("Texto de ejemplo");
+
+console.log(20);
+
+console.log(1.5);
+
+console.log(1,63);
+
+console.log(1+2);
+
+console.log(true);
+
+console.log(false);
